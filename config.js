125bd752c04a85d885249013c2c2233a82debb55
chandrashekar <139521045+chandu12121@users.noreply.github.com>
2025-02-10 21:48:25 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 module.exports = {
-    mongoURI: "mongodb+srv://chandu12121:<9182500800>@cluster0.vm2zu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+    mongoURI:"mongodb+srv://chandu12121:9182500800@cluster0.vm2zu.mongodb.net/contactsDB?retryWrites=true&w=majority"
+
   };
   
